33d59060d4a9a3fafa130a8537d33642fe3cda2d
Zakaria Essbaa <zessbaa@fum.de>
2018-05-02 14:49:39 +0200

    Added firebase authentication to the project.

--- app/src/main/java/de/fra_uas/egoe/codered/LoginActivity.java
+++ app/src/main/java/de/fra_uas/egoe/codered/LoginActivity.java
@@ -1,28 +1,25 @@
 package de.fra_uas.egoe.codered;
 
-import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class LoginActivity extends AppCompatActivity {
 
-    private Button signupButton;
+    private EditText loginEmailText, loginPwdText;
+    private Button loginBtn, registerBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        signupButton = (Button) findViewById(R.id.login_signup_btn);
+        loginEmailText = findViewById(R.id.login_email_label);
+        loginPwdText = findViewById(R.id.login_pwd_label);
+        loginBtn = findViewById(R.id.login_btn);
+        registerBtn = findViewById(R.id.login_signup_btn);
 
-        signupButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent signUp = new Intent(LoginActivity.this, ChooseRoleActivity.class);
-                startActivity(signUp);
-            }
-        });
+
     }
 }
--- app/src/main/java/de/fra_uas/egoe/codered/MainActivity.java
+++ app/src/main/java/de/fra_uas/egoe/codered/MainActivity.java
@@ -1,7 +1,11 @@
 package de.fra_uas.egoe.codered;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+
+    }
+
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        FirebaseUser currentUser = FirebaseAuth.getInstance().getCurrentUser();
+        if(currentUser == null){
+            Intent loginIntent = new Intent(MainActivity.this, LoginActivity.class);
+            startActivity(loginIntent);
+            finish();
+        }
+
     }
 }
