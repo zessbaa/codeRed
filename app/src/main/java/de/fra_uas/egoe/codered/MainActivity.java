811b9e8bd65a60068f556072061a8c248796b7a5
Zakaria Essbaa <zessbaa@fum.de>
2018-05-03 12:42:47 +0200

    Added action menu to the toolbar.

--- app/src/main/java/de/fra_uas/egoe/codered/MainActivity.java
+++ app/src/main/java/de/fra_uas/egoe/codered/MainActivity.java
@@ -3,18 +3,26 @@ package de.fra_uas.egoe.codered;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Toolbar dashboardToolbar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        dashboardToolbar = findViewById(R.id.sender_dashboard_toolbar);
+        setSupportActionBar(dashboardToolbar);
 
+        getSupportActionBar().setTitle("Dashboard");
 
     }
 
@@ -30,5 +38,13 @@ public class MainActivity extends AppCompatActivity {
             finish();
         }
 
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+
+        return true;
     }
 }
